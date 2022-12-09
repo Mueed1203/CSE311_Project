e4b5677a2a206380cfe9a4705a88de63b66a3230
Mueed <abdul.mueed@northsouth.edu>
2022-12-10 00:41:30 +0600

    Testing database relations with a dummy user

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
-  .then(result => {
+ req.user
+ .createProduct({
+  title: title,
+  price: price,
+  imageUrl: imageUrl,
+  description: description,
+  userId: req.user.id
+})
+ .then(result => {
    // console.log(result);
      console.log('Created product');
      res.redirect('/admin/products')
@@ -35,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user
+  .getProducts({where: {id: prodId}})
+  //Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -73,7 +77,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  .getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
